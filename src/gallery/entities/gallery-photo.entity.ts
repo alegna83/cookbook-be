84f55dddcb14d77442c5aee660c41210e8e8ebcc
alegna83 <angelalopesantunes@gmail.com>
2026-04-22 16:40:42 +0100

    refactor(gallery): map photo columns to snake_case

--- src/gallery/entities/gallery-photo.entity.ts
+++ src/gallery/entities/gallery-photo.entity.ts
@@ -3,7 +3,7 @@ import {
   PrimaryGeneratedColumn,
   Column,
   ManyToOne,
-  //JoinColumn,
+  JoinColumn,
 } from 'typeorm';
 import { Accommodation } from 'src/accommodations/entities/accommodation.entity';
 
@@ -12,11 +12,12 @@ export class GalleryPhoto {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ nullable: true })
+  @Column({ name: 'photo_url', nullable: true })
   url: string;
 
   @ManyToOne(() => Accommodation, (place) => place.gallery_photos, {
     onDelete: 'CASCADE',
   })
+  @JoinColumn({ name: 'place_id' })
   place: Accommodation;
 }
